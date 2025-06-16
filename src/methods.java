b128afec9d6ed09e3b080ab9dfeb5af3b2e41ad5
miss-world-09 <disha.sahay2005@gmail.com>
2025-06-17 00:51:02 +0530

    Update methods.java

--- src/methods.java
+++ src/methods.java
@@ -1,1 +1,11 @@
-
+import java.util.Arrays;
+public class Changevalues{
+    public static void main(String[] args){
+        int[] arr={4,5,6};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void change(int[] nums){
+        nums[0]=66;
+    }
+}
