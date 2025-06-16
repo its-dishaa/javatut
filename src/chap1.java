9a4595d3fcc3b18a874c596e7d608f376f259a99
Disha <disha.sahay2005@gmail.com>
2025-06-16 21:37:23 +0530

    CH3

--- src/chap1.java
+++ src/chap1.java
@@ -1,3 +1,29 @@
+//VARIABLES AND DATABASE:-
+/* VARIABLE:- A VARIABLE IS A CONTAINER THAT STORES A VALUE. THIS VALUE CAN BE CHANGED DURING EXECUTION OF PROG.
+EX:- int num=8;
+num is variable amd int is data type
+
+DATA TYPES:- THERE ARE TWO TYPES OF DATA TYPES:-
+
+ PRIMITIVE DATA TYPE:- VARIABLE DECLARED BEFORE USE. THERE ARE 8 PRIMITIVE DATA TYPES:
+1. byte:- takes 1 byte
+2. short:- takes 2 bytes
+3. int:- takes 4 bytes
+4. float:- takes 4 bytes
+5. long:- takes 8 bytes
+6. double:- takes 8 bytes
+7. char:- takes 2 bytes
+8. boolean:- size depends on jvm
+
+LITERALS:- A CONSTANT VALUE WHICH CAN BE ASSIGNED TO THE VARIABLE.
+KEYWORDS:- WORDS WHICH ARE RESERVED.
+
+READING DATA FROM KEYWORD:
+Scanner S=new Scanner(System.in);
+
+ */
+
+
 public class chap1 {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
