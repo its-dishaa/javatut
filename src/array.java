0da4cf963bea9993ca44befb8b3a1a18261d3912
miss-world-09 <disha.sahay2005@gmail.com>
2025-07-04 16:27:05 +0530

    Update array.java

--- src/array.java
+++ src/array.java
@@ -56,6 +56,37 @@ public class Main {
         str[1] = "kunal";
         System.out.println(Arrays.toString(str));
 
+// 2-D Array:- Two Dimensional Array.
+class Main {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2},
+            {1, 2, 3},
+            {5, 7, 8}
+        };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println(); 
+        }
+    }
+}
+
+/*ArrayList:- An ArrayList in Java is a resizable array — unlike regular arrays, which have a fixed size, an ArrayList can grow or shrink as needed.
+Think of it like a dynamic list that can hold many elements and automatically manages its size.
+SYNATX */
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+       ArrayList<Integer> list=new ArrayList<>();
+    }
+}
+
+
+
 
 
 
