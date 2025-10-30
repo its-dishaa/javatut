7954c992c24a2d5190fd7800a0cd315018456927
Disha <disha.sahay2005@gmail.com>
2025-10-31 01:25:57 +0530

    Update Sorting.java

--- src/Sorting.java
+++ src/Sorting.java
@@ -25,3 +25,26 @@ public class Main {
         }
     }
 }
+
+
+//Selection Sort
+class Main {
+    public static void main(String[] args) {
+        int[] arr={10,60,40,20};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selectionSort(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            int minindex=i;
+            for(int j=1;j<arr.length;j++){
+                if(arr[j]<arr[minindex]){
+                    minindex=j;
+                }
+            }
+            int temp=arr[minindex];
+            arr[minindex]=arr[i];
+            arr[i]=temp;
+        }
+    }
+}
